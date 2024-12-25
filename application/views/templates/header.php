825af5de3f1c47830eac40e2883e0a90aeab012d
MerrrH1 <mervinhowardd38@gmail.com>
2024-12-25 22:17:18 +0700

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css'); ?>">
+    <script src="<?= base_url('assets/js/bootstrap.js'); ?>"></script>
+    <title></title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="<?= base_url(); ?>">BookHUB</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="<?= base_url(); ?>">Beranda</a>
+                    <a class="nav-link" href="#">Buku</a>
+                    <a class="nav-link" href="#">Peminjaman</a>
+                    <a class="nav-link" href="#">Ulasan</a>
+                    <a class="nav-link" href="#">Kategori</a>
+                    <a class="nav-link" href="#">Member</a>
+                </div>
+            </div>
+            <div class="d-flex">
+                <!-- Login -->
+            </div>
+        </div>
+    </nav>
+    <div class="container my-3">
